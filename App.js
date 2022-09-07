c85ad644bc1f3c0946d3bd998619a32af626a0d8
phunnama <pm.phunnama31@gmail.com>
2022-09-07 10:58:10 +0700

    7-9-2022(new)

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Butto, Image } from "react-native";
+import { StyleSheet, Text, View, Butto, Image,SafeAreaView } from "react-native";
 import React from "react";
 
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
