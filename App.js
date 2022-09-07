019662389569309949915d9715cebad756d78f4b
phunnama <pm.phunnama31@gmail.com>
2022-09-07 10:51:24 +0700

    7-9-2022

--- App.js
+++ App.js
@@ -1,62 +1,104 @@
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Butto, Image } from "react-native";
 import React from "react";
 
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { 
+import {
   createDrawerNavigator,
   DrawerContentScrollView,
   DrawerItemList,
-  DrawerItem
+  DrawerItem,
 } from "@react-navigation/drawer";
 
-function FeedScreen() {
+import HomeScreen from "./screens/HomeScreen";
+import { Ionicons } from "@expo/vector-icons";
+import {
+  HeaderButtons,
+  HeaderButton,
+  Item,
+  HiddenItem,
+  OverflowMenu,
+} from "react-navigation-header-buttons";
+import { SafeAreaView } from "react-native-web";
+
+const MyTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "rgb(255,45,85)",
+  },
+};
+scroll;
+function FeedScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Feed Screen</Text>
+      <Button title="open drawer" onPress={() => navigation.openDrawer()} />
+      <Button title="toggle drawer" onPress={() => navigation.toggleDrawer()} />
     </View>
   );
 }
 
-function ArticleScreen() {
+function NotificationsScreen() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Article Screen</Text>
+      <Text>Notifications Screen</Text>
     </View>
   );
 }
 
-function CustomDrawerContent(props){
-  return(
-    <DrawerContentScrollView {...props}>
-      <DrawerItemList {...props}/>
-      <DrawerItem label='Help' onPress={()=> alert('Link to help')} />
-    </DrawerContentScrollView>
-  )
+function CustomDrawerContent(props) {
+  return (
+    <SafeAreaView>
+      <DrawerContentScrollView {...props}>
+        <Image
+          style={styles.sideMenuProfileIcon}
+          source={require("./assets/React_logo.png")}
+        />
+        <DrawerItemList {...props} />
+        <DrawerItem
+          label="Close Drawer"
+          onPress={() => props.navigation.closeDrawer()}
+        />
+      </DrawerContentScrollView>
+    </SafeAreaView>
+  );
 }
 const Drawer = createDrawerNavigator();
 
-function MyDreawer(){
-  return(
-    <Drawer.Navigator useLegacyImplementation
-    drawerContent={(props)=><CustomDrawerContent{...props}/>}
+function MyDrawer() {
+  return (
+    <Drawer.Navigator
+      useLegacyImplementation
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
       screenOptions={{
-        drawerStyle:{
-          backgroundColor:'pink',
-          width:240
-        }
-      }}>
-        <Drawer.Screen name="Feed" component={FeedScreen} />
-        <Drawer.Screen name="Article" component={ArticleScreen} />
-      </Drawer.Navigator>
-  )
+        drawerStyle: {
+          //backgroundColor: '',
+          width: 240,
+        },
+      }}
+    >
+      <Drawer.Screen name="Home" component={HomeScreen} />
+      <Drawer.Screen name="Notification" component={NotificationsScreen} />
+    </Drawer.Navigator>
+  );
 }
 const App = () => {
   return (
-    <NavigationContainer>
-      <MyDreawer/>
+    <NavigationContainer theme={MyTheme}>
+      <MyDrawer />
     </NavigationContainer>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  sideMenuProfileIcon: {
+    resizeMode: "center",
+    width: 100,
+    height: 100,
+    borderRadius: 100 / 2,
+    alignSelf: "center",
+  },
+});
