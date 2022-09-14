d90605411804ce24b7aeffe402dc18003920bc61
phunnama <pm.phunnama31@gmail.com>
2022-09-14 11:56:38 +0700

    drawe+tab

---
+++ App(new).js
@@ -0,0 +1,47 @@
+import { View, Text,ActivityIndicator,FlatList } from 'react-native'
+import React,{useEffect,useState} from 'react'
+
+const App = () => {
+
+    const[isLoading,setLoading] = useState(true);
+    const [data,setData] =useState([]);
+
+    const getMoive = async() => {
+      try{
+        const reponse = await fetch('https://reactnative.dev/movies.json')
+        const json = await reponse.json();
+        setData(json.movies)
+      }
+      catch(error){
+        alert(error.message);
+      }
+      finally{
+        setLoading(false);
+      }
+    }
+
+    useEffect(()=>{
+      getMoive();
+
+    },[])
+
+    return (
+    <View style={{flex:1,padding:20}}>
+      { isLoading ? <ActivityIndicator/> : (
+        <FlatList
+        data = {data}
+        keyExtractor = {({id},index)=>id} 
+        renderItem={({item})=>(
+          <Text>{item.title}, {item.releaseYear}</Text>
+
+        )}
+
+        />
+      )
+
+      }
+    </View>
+  );
+}
+
+export default App
--- App.js
+++ App.js
@@ -1,47 +1,125 @@
-import { View, Text,ActivityIndicator,FlatList } from 'react-native'
-import React,{useEffect,useState} from 'react'
+import { StyleSheet, Text, View, Button, SafeAreaView } from "react-native";
+import React from "react";
 
-const App = () => {
+import { NavigationContainer,DefaultTheme } from "@react-navigation/native";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from "@react-navigation/drawer";
+import {
+  HeaderButtons,
+  HeaderButton,
+  Item,
+  HiddenItem,
+  OverflowMenu,
+} from "react-navigation-header-buttons";
 
-    const[isLoading,setLoading] = useState(true);
-    const [data,setData] =useState([]);
+const MyTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "rgb(255,45,85)",
+  },
+};
 
-    const getMoive = async() => {
-      try{
-        const reponse = await fetch('https://reactnative.dev/movies.json?fbclid=IwAR1TcpbE4RAcWjvRvdD8E4NJLfhbi9O5YJl-l3F9uqQY75OpbxJIo5hZSuI')
-        const json = await reponse.json();
-        setData(json.movies)
-      }
-      catch(error){
-        alert(error.message);
-      }
-      finally{
-        setLoading(false);
-      }
-    }
+const IoniconsHeaderButton = (props) => (
+  // the props here come from <Item ... />
+  // you may access them and pass something else to HeaderButton if you like
+  <HeaderButton IconComponent={Ionicons} iconSize={23} {...props} />
+);
 
-    useEffect(()=>{
-      getMoive();
+const HomeScreen = ({navigation}) => {
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      // in your app, you can extract the arrow function into a separate component
+      // to avoid creating a new one every time you update the options
+      headerRight: () => (
+        <HeaderButtons HeaderButtonComponent={IoniconsHeaderButton}>
+          <Item title="register" iconName="person-add" onPress={() => alert('Register')} />
+        </HeaderButtons>
+      ),
+    });
+  }, [navigation]);
 
-    },[])
+  
 
-    return (
-    <View style={{flex:1,padding:20}}>
-      { isLoading ? <ActivityIndicator/> : (
-        <FlatList
-        data = {data}
-        keyExtractor = {({id},index)=>id} 
-        renderItem={({item})=>(
-          <Text>{item.title}, {item.releaseYear}</Text>
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Ionicons name='home' size={30} color='#008b8b'/>
+    <Text>Home Screen</Text>
+    <Button title='Open Drawer'
+    onPress={()=> navigation.openDrawer()}/>
+  </View>
+  )
+}
 
-        )}
-
-        />
-      )
-
-      }
+function SettingScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Settings!</Text>
+      <Button title="go to home" onPress={() => navigation.navigate("Home")} />
     </View>
   );
 }
 
-export default App
+const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === "Home") {
+            iconName = focused
+              ? "ios-information-circle"
+              : "ios-information-circle-outline";
+          } else if (route.name === "Setting") {
+            iconName = focused ? "ios-list" : "ios-list-outline";
+          }
+          //you can return any conponent that you like here
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "tomato",
+        tabBarInactiveTintColor: "gray",
+      })}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Setting" component={SettingScreen} />
+    </Tab.Navigator>
+  );
+}
+
+function MyDrawer() {
+  return(
+    <Drawer.Navigator>
+      <Drawer.Screen name="Home" component={MyTabs} />
+      <Drawer.Screen name="Setting" component={SettingScreen} />
+    </Drawer.Navigator>
+  );
+}
+
+const App = () => {
+  return (
+    <NavigationContainer theme={MyTheme} >
+      <MyDrawer />
+    </NavigationContainer>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  sideMenuProfileIcon: {
+    resizeMode: "center",
+    width: 100,
+    height: 100,
+    borderRadius: 100 / 2,
+    alignSelf: "center",
+  },
+});
